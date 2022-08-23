222879f5a7d03399e65edc39d9ffa46338917829
Junior Oliveira <junioroliveira@hotmail.com>
2022-08-22 23:01:59 -0300

    Adicionar arquivos de projeto.

---
+++ INIFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update
+{
+    public class INIFile
+    {
+        public string path
+        {
+            get; private set;
+        }
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+        public INIFile(string INIPath = null)
+        {
+            //path = INIPath;
+            string DiretorioINI = Directory.GetCurrentDirectory() + @"\setting.ini";
+            if (!File.Exists(DiretorioINI))
+            {
+                File.Create(DiretorioINI);
+            }
+            path = Directory.GetCurrentDirectory() + @"\setting.ini";
+        }
+        public void IniWriteValue(string Section, string Key, string Value)
+        {
+            WritePrivateProfileString(Section, Key, Value, this.path);
+        }
+        public string IniReadValue(string Section, string Key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(Section, Key, "", temp, 255, this.path);
+            return temp.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Update
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static string programa;
+
+
+        static void Main(string[] args)
+        {
+
+            const int SW_HIDE = 0;
+            const int SW_SHOW = 5;
+            var handle = GetConsoleWindow();
+
+            // Ocultar
+            ShowWindow(handle, SW_HIDE);
+
+            // Mostrar
+            //ShowWindow(handle, SW_SHOW);
+
+            Thread.Sleep(500);
+
+            INIFile ini = new INIFile();
+            programa = Directory.GetCurrentDirectory() + @"\" + ini.IniReadValue("Programa", "Executavel");
+
+            string PathFilesOrigem = Directory.GetCurrentDirectory() + @"\Temp\";
+            string PathDestino = Directory.GetCurrentDirectory() + @"\";
+            
+            if (mover(PathFilesOrigem, PathDestino))
+            {
+                limpar(PathFilesOrigem);
+                executar();
+            }
+        }
+
+        private static void limpar(string origem)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(origem);
+
+                foreach (FileInfo f in dir.GetFiles())
+                {
+                    File.Delete(f.FullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static bool mover(string origem, string destino)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(origem);
+
+                foreach (FileInfo f in dir.GetFiles())
+                {
+                    File.Move(f.FullName, destino + f.Name, true);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public static void executar()
+        {
+            System.Diagnostics.Process.Start(programa);
+            Environment.Exit(0);
+        }
+    }
+}
